885537bb6db561bb244276b6eaeb1ea54bfb28db
Julesntizimira <ntizimijules5@gmail.com>
2024-01-04 11:45:00 +0000

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import {uploadPhoto, createUser} from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([
+    uploadPhoto(),
+    createUser(),
+  ]).then((messages) => {
+    console.log(
+        `${messages[0].body} ` +
+      `${messages[1].firstName} ` +
+      `${messages[1].lastName}`);
+  });
+}
