0865758f4a888f1d05bc40c29563f1f3d57939a1
Julesntizimira <ntizimijules5@gmail.com>
2024-01-09 23:26:15 +0200

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,5 +2,9 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
-  return [...set].filter((x) => x.startsWith(startString)).map((x) => x.replace(startString, '')).join('-');
+  const filteredValue = [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.substring(startString.length))
+    .join('-');
+  return filteredValue;
 }
