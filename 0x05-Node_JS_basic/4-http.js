0a46035b297afe2e651bb747c6ae8f92aef83dc4
Julesntizimira <ntizimijules5@gmail.com>
2024-02-27 14:57:49 +0200

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,15 +4,7 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  // Set response headers
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  // Routing
-  if (req.url === '/') {
-    // Handle root route
-    res.end('Welcome to the homepage!');
-  }
+  res.end('Welcome to the homepage!');
 });
 
 app.listen(port, hostname, () => {
