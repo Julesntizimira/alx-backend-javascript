b593a7f23376ee39520f0bd4e293716682099b52
Julesntizimira <ntizimijules5@gmail.com>
2024-02-27 15:03:31 +0200

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,12 +5,12 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   // Setting the status code and content type
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // Sending the response body
+
   res.end('Hello Holberton School!\n');
 });
 
 // Listening on port 1245
-app.listen(1245);
+app.listen(1245, () => {});
 
 // Exporting the app variable
 module.exports = app;
