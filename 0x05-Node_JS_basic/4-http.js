f652b6922c95f4ea5c34d3557884a58ec5097c0f
Julesntizimira <ntizimijules5@gmail.com>
2024-02-27 14:55:56 +0200

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -16,5 +16,5 @@ const app = http.createServer((req, res) => {
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log('...');
 });
