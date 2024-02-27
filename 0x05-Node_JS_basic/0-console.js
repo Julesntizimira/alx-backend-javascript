ad30a6fd8ac8a222f74bcf37a5bb967267caa145
Julesntizimira <ntizimijules5@gmail.com>
2024-02-27 11:36:44 +0200

    nodejs bacis

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
-/* Executing basic javascript with Node JS 
+/* Executing basic javascript with Node JS
 */
 function displayMessage(arg) {
-    console.log(arg);
+  console.log(arg);
 }
 module.exports = displayMessage;
