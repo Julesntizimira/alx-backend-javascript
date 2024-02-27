7170ce41077faaadce6a3b2494abae7381920834
Julesntizimira <ntizimijules5@gmail.com>
2024-02-27 14:09:43 +0200

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,37 @@
-/* Reading a file synchronously with Node JS
-*/
 const fs = require('fs');
 
-function countStudents(path) {
+function parseCSVLine(line) {
+  const [firstName, lastName, age, field] = line.split(',');
+  return {
+    firstName, lastName, age: parseInt(age), field,
+  };
+}
+
+function countStudents(filePath) {
   try {
     // Read file synchronously
-    let data = fs.readFileSync(path, 'utf8');
-    data = data.split('\n').slice(1);
-    const lineCount = data.length;
-    console.log(`Number of students: ${lineCount}`);
-    const swe = [];
-    const cs = [];
-    for (const line of data) {
-      if (line !== '\n') {
-        const lineSplited = line.split(',');
-        if (lineSplited[3] === 'SWE') swe.push(lineSplited[0]);
-        else if (lineSplited[3] === 'CS') cs.push(lineSplited[0]);
-      }
-    }
-    const fields = [{ name: 'CS', data: cs }, { name: 'SWE', data: swe }];
-    for (const field of fields) {
-      console.log(`Number of students in ${field.name}: ${field.data.length}. List: ${field.data.join(', ')}`);
+    const data = fs.readFileSync(filePath, 'utf8').split('\n').slice(1);
+
+    // Parse CSV lines and filter out empty lines
+    const students = data.filter((line) => line.trim() !== '').map(parseCSVLine);
+
+    // Count total number of students
+    console.log(`Number of students: ${students.length}`);
+
+    // Group students by field
+    const groupedStudents = students.reduce((groups, student) => {
+      groups[student.field] = groups[student.field] || [];
+      groups[student.field].push(`${student.firstName} ${student.lastName}`);
+      return groups;
+    }, {});
+
+    // Output number of students in each field
+    for (const [field, studentsList] of Object.entries(groupedStudents)) {
+      console.log(`Number of students in ${field}: ${studentsList.length}. List: ${studentsList.join(', ')}`);
     }
   } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
+
 module.exports = countStudents;
