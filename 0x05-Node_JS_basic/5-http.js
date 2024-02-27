15a1698eff7dbc83721a2d384fd9e247cf4e3ea0
Julesntizimira <ntizimijules5@gmail.com>
2024-02-27 16:53:17 +0200

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,32 +1,48 @@
-const http = require('http')
-const countStudents = require('./3-read_file_async.js');
-const {spawn} = require('child_process');
+const http = require('http');
+const fs = require('fs').promises;
 
-
-const arg = process.argv[2]
-
-const port = 1245;
+async function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8')
+      .then((db) => {
+        let data;
+        const students = db.split('\n').filter((line) => line.trim() !== '');
+        data = `Number of students: ${students.length - 1}`;
+        const fields = {};
+        students.slice(1).forEach((student) => {
+          const field = student.split(',')[3];
+          const name = student.split(',')[0];
+          if (fields[field]) {
+            fields[field].push(name);
+          } else {
+            fields[field] = [name];
+          }
+        });
+        Object.keys(fields).forEach((key) => {
+          data = data.concat(`\nNumber of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
+        });
+        resolve(data);
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
+}
 
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    if (req.url === '/') res.end('Hello Holberton School!');
-    
-    else if (req.url === '/students') {
-        const child = spawn('node', [countStudents, arg])
-        const result = ['This is the list of our students']
-        child.on('error', (error) => {
-                result.push(error.message)
-                res.end(result.join('\n'));
-        });
-        child.stderr.on('data', (data) => {
-            result.push(`${data}`)
-            res.end(result.join('\n'));
-        });
-        child.stdout.on('data', (data) => {
-                result.push(`${data}`)
-                res.end(result.join('\n'));
-        })
-    }   
-})
-app.listen(port, () => {});
-module.exports = app
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((err) => {
+        res.end(`This is the list of our students\n${err.message}`);
+      });
+  }
+});
+
+app.listen(1245, () => {});
+module.exports = app;
