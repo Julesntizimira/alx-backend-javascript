74a09aadcd622f7898e22d3ea3ec93cbeba05eec
Julesntizimira <ntizimijules5@gmail.com>
2024-02-29 22:58:05 +0200

    uniitestJs

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,25 @@
-import { assert } from 'chai';
-import calculateNumber from './0-calcul.js';
+const assert = require("assert");
+const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', () => {
-  it('case1', () => {
-    assert(calculateNumber(1, 3), 4);
+  it('it round the first argument', () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
   });
-  it('case2', () => {
-    assert(calculateNumber(1, 3.7), 5);
+
+  it('it round the second argument', () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
   });
-  it('case3', () => {
-    assert(calculateNumber(1.2, 3.7), 5);
-  });
-  it('case4', () => {
-    assert(calculateNumber(1.5, 3.7), 6);
+
+  it('it should return the right number', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
   });
 });
